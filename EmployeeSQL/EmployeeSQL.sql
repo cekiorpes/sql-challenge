1f17731df8546ad71e991ee881248028a4f58383
Caroline Kiorpes <cekiorpes@gmail.com>
2022-02-15 15:05:31 -0500

    Update EmployeeSQL.sql

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -1,19 +1,37 @@
-DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS employees, departments, titles, dept_emp, dept_manager, salaries;
+
+CREATE TABLE titles (
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR
+);
 
 CREATE TABLE employees (
 	emp_no INTEGER PRIMARY KEY,
-	emp_title VARCHAR,
+	emp_title VARCHAR(5),
 	birth_date DATE,
-	first_name TEXT,
-	last_name TEXT,
+	first_name VARCHAR,
+	last_name VARCHAR,
 	sex TEXT,
 	hire_date DATE
+	FOREIGN KEY (emp_title) REFERENCES titles(title_id)
 );
-
-DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
-	dept_no VARCHAR PRIMARY KEY,
-	dept_name TEXT
+	dept_no VARCHAR(4) PRIMARY KEY,
+	dept_name VARCHAR
 );
 
+CREATE TABLE dept_emp (
+	emp_no INTEGER,
+	dept_no VARCHAR(5)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(5),
+	emp_no INTEGER
+);
+
+CREATE TABLE salaries (
+	emp_no INTEGER,
+	salary INTEGER
+);
