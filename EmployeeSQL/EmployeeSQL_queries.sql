1477516e0746c9ce004baf9bfcb917d8b98ce5ee
Caroline Kiorpes <cekiorpes@gmail.com>
2022-02-15 15:22:35 -0500

    Create EmployeeSQL_queries.sql

---
+++ EmployeeSQL/EmployeeSQL_queries.sql
@@ -0,0 +1,11 @@
+--Employee details (employee number, last name, first name, sex, and salary)
+SELECT emp_no, last_name, first_name, sex, salary
+FROM employees e
+	INNER JOIN salaries s on e.emp_no = s.emp_no;
+	
+--Employees hired in 1986 (first name, last name, hire date)
+SELECT e.first_name, e.last_name, e.hire_date
+WHERE EXTRACT (year FROM hire_date) = 1986
+--Where hire_date LIKE %1986?
+
+--Managers of departments (department number, department name, employee number, last name, first name)
