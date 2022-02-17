48cf6b7630b9b602f60d402392cd1ca3c2f91fdc
Caroline Kiorpes <cekiorpes@gmail.com>
2022-02-16 20:09:37 -0500

    Update EmployeeSQL_queries.sql

--- EmployeeSQL/EmployeeSQL_queries.sql
+++ EmployeeSQL/EmployeeSQL_queries.sql
@@ -1,10 +1,11 @@
 --Employee details (employee number, last name, first name, sex, and salary)
-SELECT emp_no, last_name, first_name, sex, salary
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 	INNER JOIN salaries s ON e.emp_no = s.emp_no;
 	
 --Employees hired in 1986 (first name, last name, hire date)
 SELECT e.first_name, e.last_name, e.hire_date
+FROM employees e
 WHERE EXTRACT (year FROM hire_date) = 1986;
 --Where hire_date LIKE %1986?
 
